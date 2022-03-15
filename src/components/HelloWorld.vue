ff06a804b136a0d6a2699a420c7c38d767237070
yuusheng <contact@yuusheng.cn>
2022-03-15 13:19:43 +0800

    vite init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>this is hello world component</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
