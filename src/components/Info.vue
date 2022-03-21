a989568d62a372cb564a95520e1acfec91a60f47
yuusheng <contact@yuusheng.cn>
2022-03-21 20:12:01 +0800

    dark logo

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,7 +2,9 @@
   <div
     class="absolute right-24 top-20 rounded-xl shadow-xl w-52 flex flex-col bg-white p-3 border-[2px] border-gray-100"
   >
-    <a href="/user" class="mx-auto py-2 my-1 text-lg font-bold border-b-[1px] border-gray-200/60">{{ name }}</a>
+    <a href="/user" class="mx-auto py-2 my-1 text-lg font-bold border-b-[1px] border-gray-200/60 dark:text-black">{{
+      name
+    }}</a>
     <section class="flex justify-center flex-col mt-2 mb-4">
       <a href="/user" class="section-item">个人中心</a>
       <a href="#" class="section-item">投稿管理</a>
@@ -30,6 +32,6 @@ const logout = () => {
 
 <style scoped>
 .section-item {
-  @apply hover:bg-gray-200/60 rounded px-2 py-1 cursor-pointer transition-all duration-200;
+  @apply hover:bg-gray-200/60 rounded px-2 py-1 cursor-pointer transition-all duration-200 dark:text-black;
 }
 </style>
