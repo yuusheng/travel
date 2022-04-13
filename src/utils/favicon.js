7aeaa8b4b1085918b7c4065c4c75b074e3be9b7e
yuusheng <contact@yuusheng.cn>
2022-04-13 19:47:59 +0800

    deploy website

---
+++ src/utils/favicon.js
@@ -0,0 +1,16 @@
+import { ref, watch } from 'vue'
+export default function useFavicon(newIcon) {
+  const favicon = ref(newIcon)
+
+  const updateIcon = (icon) => {
+    document.head
+      .querySelectorAll(`link[ref*="icon"]`)
+      .forEach((el) => (el.href = `${icon}`))
+  }
+
+  const reset = () => (favicon.value = '/favicon.ico')
+  watch(favicon, (i) => {
+    updateIcon(i)
+  })
+  return { favicon, reset }
+}
