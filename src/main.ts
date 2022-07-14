5e1b5fac11080aa0287cee88191c8daae03f2f76
yuusheng <contact@yuusheng.cn>
2022-07-14 18:20:03 +0800

    fix: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import './index.css'
 import directives from './utils/directives'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
-app.use(router).use(directives).use(store).mount('#app')
+
+const pinia = createPinia()
+app.use(router).use(directives).use(pinia).mount('#app')
